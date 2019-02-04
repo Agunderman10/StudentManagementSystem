03d498619087e3a92112ba4a839a4f5c98c37d92
Andrew Gunderman <agunderman10@gmail.com>
2019-02-04 18:54:34 -0500

    Added private members

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main 
 {
+	private String studentName;
+	private int studentAge;
+	private String gender;
 
 	public static void main(String[] args) 
 	{
-
+		System.out.println("Hello! What is your name?");
 	}
 
 }
