3047855cdbe29fd7ae94032532ab30f208ea19f1
Andrew Gunderman <agunderman10@gmail.com>
2019-02-04 19:06:28 -0500

    Moved variables

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main 
 {
-	private String studentName;
-	private int studentAge;
-	private String gender;
 
 	public static void main(String[] args) 
 	{
+		String studentName;
+		int studentAge;
+		String studentGender;
+		
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Hello! What is your name?");
