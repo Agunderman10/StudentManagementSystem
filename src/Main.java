4f1e33c51ee24123767ee233f054b5d7d99f9015
lexinevin <lexinevin@gmail.com>
2019-02-04 19:28:16 -0500

    Added 2 variables & Testing fork

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-public class Main 
+public class Main
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		String studentName;
 		int studentAge;
 		String studentGender;
-		
+		int studentGrade;
+		double studentGpa; /* can be calculated later */
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("Hello! What is your name?");
 	}
 
