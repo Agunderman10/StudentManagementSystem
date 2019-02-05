419909382e152ba0bccddf5e3105a6e215558cff
Andrew Gunderman <agunderman10@gmail.com>
2019-02-05 04:45:04 -0500

    Added additional questions

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,23 @@ public class Main
 		String studentName;
 		int studentAge;
 		String studentGender;
-		int studentGrade;
+		int studentNumberOfClasses;
 		double studentGpa; /* can be calculated later */
 
-		Scanner scan = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Hello! What is your name?");
+		System.out.println("Please enter your name.");
+		studentName = scanner.next();
+		
+		System.out.println("Please enter your age.");
+		studentAge = scanner.nextInt();
+		
+		System.out.println("Please enter your gender in the format of male, female, or other.");
+		studentGender = scanner.next(); 
+		
+		System.out.println("How many classes do you have?");
+		studentNumberOfClasses = scanner.nextInt();
+		
 	}
 
 }
