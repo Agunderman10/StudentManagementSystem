dcb45a517ec467117966aec3d6724380c867c1ea
Andrew Gunderman <agunderman10@gmail.com>
2019-02-05 05:09:28 -0500

    Added class names array

--- src/Main.java
+++ src/Main.java
@@ -25,20 +25,28 @@ public class Main
 		System.out.println("How many classes do you have?");
 		studentNumberOfClasses = scanner.nextInt();
 		
-		//init array that will contain the student's grades
+		//init arrays that will contain student class grades and names
+		String[] studentClassNames = new String[studentNumberOfClasses];
 		int[] studentGrades = new int[studentNumberOfClasses];
 		
 		//ask the student for their grade in each of their classes depending on how many classes they said they have
 		//we take each grade and add it to the studentGrades array
+		//ask the student for their class names and add the names to a class name array
 		for(int i = 1; i <= studentNumberOfClasses; i++) 
 		{
+			System.out.println("What is the name of class " + i + "?");
+			String name = scanner.next();
 			System.out.println("What is your grade for class " + i + "?");
 			int grade = scanner.nextInt();
 			
 			//i - 1 because in the for loop i starts at 1 and not 0. We use i - 1 to keep the array index from
 			//going out of bounds
+			studentClassNames[i - 1] = name;
 			studentGrades[i - 1] = grade;
 		}
+		
+		//close scanner
+		scanner.close();
 	}
 
 }
