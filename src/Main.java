d54d19aeb11a50bc05674a3d2cb44e663510fc9f
lexinevin <lexinevin@gmail.com>
2019-02-05 22:12:31 -0500

    Testing a commit through git bash

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//this is a test
 public class Main
 {
 
@@ -15,34 +15,34 @@ public class Main
 		ArrayList<Double> studentGrades = new ArrayList<>();
 
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Please enter your name.");
 		studentName = scanner.next();
-		
+
 		System.out.println("Please enter your age.");
 		studentAge = scanner.nextInt();
-		
+
 		System.out.println("Please enter your gender in the format of male, female, or other.");
-		studentGender = scanner.next(); 
-		
+		studentGender = scanner.next();
+
 		System.out.println("How many classes do you have?");
 		studentNumberOfClasses = scanner.nextInt();
-		
+
 		//ask the student for their grade in each of their classes depending on how many classes they said they have
 		//we take each grade and add it to the studentGrades arraylist
 		//ask the student for their class names and add the names to a class name arraylist
-		for(int i = 1; i <= studentNumberOfClasses; i++) 
+		for(int i = 1; i <= studentNumberOfClasses; i++)
 		{
 			System.out.println("What is the name of class " + i + "?");
 			String name = scanner.next();
 			System.out.println("What is your grade for class " + i + "?");
 			double grade = scanner.nextDouble();
-			
+
 			//add class names and grades to the names and grades arraylists
 			studentClassNames.add(name);
 			studentGrades.add(grade);
 		}
-		
+
 		//close scanner
 		scanner.close();
 	}
